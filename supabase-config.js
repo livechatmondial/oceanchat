83dc5426961a0da38a39d6b985079cb24c03d0cd
oceanchat <chaabanetekerrabet@gmail.com>
2026-09-02 22:39:39 -0400

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+```js
+// 🌊 OceanChat - Configuration Supabase
+
+window.OCEANCHAT_SUPABASE_URL = "https://dkdfrlbzmzkupnxhijxe.supabase.co";
+
+window.OCEANCHAT_SUPABASE_KEY = "sb_publishable_RB8wvFQWmoRhx12wlAHYng_4pE3PE8z";
+```
+
